4a576f1252bd076f67b1e905313068f91388ed59
Hooda Akshat <hooda.akshat@syk.fi>
2022-08-22 14:00:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+       int a=10;
+       int b=15;
+       if(a==b)
+           System.out.println((a+b)*(a+b));
+       else
+           System.out.println(a+b);
+    }
+}
